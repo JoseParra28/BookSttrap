8a8d7215ef7e9baccc80ad63f5ef38e720ba10a5
JoseParra28 <iliftandcook@gmail.com>
2022-11-27 19:44:41 +0000

    initial commit, import ranmon and words

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+import WORDS
+
+word = "Secret"
+
+errors = 6 
+guesslist = []
+done = False
+
+while not done:
+    for letter in word:
+        if letter.lower() in guesslist:
+            print(letter, end=" ")
+        else:
+            print(" ", end=" ")
+    print("")
+done = True
+
