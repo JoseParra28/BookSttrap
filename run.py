499d2f46287ee90be3f13f4adf53f6bcf85ec339
JoseParra28 <iliftandcook@gmail.com>
2022-12-02 05:02:09 +0000

    commit

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
 import random
 
-words = ['programe', 'computer', 'questions']
+words = ['programe']
 
 picked = random.choice(words)
 
-print('The word has', len(picked), 'letters.')
+print('The word has', len(picked), 'letters')
 
 
 correct = ['_'] * len(picked)
 wrong = []
 
 def update():
-  for i in correct:
+   for i in correct:
     print(i, end=' ')
-  print()
+   print()
 
 update()  
 
@@ -22,11 +22,26 @@ while True:
   guess = input("Please guess a letter\n")
 
   if guess in picked:
-    correct.append(guess)
-    print('Correct',correct)
+    index = 0 
+    for i in picked:
+      if i == guess:
+        correct[index] = guess
+      index += 1
+    update()  
+   
   else:
-    wrong.append(guess)
-    print('wrong',wrong)
+      if guess not in wrong:
+          wrong.append(guess)
+      else:
+          print('You already guessed that!') 
+      print(wrong)  
+  if '_' not in correct:
+       print('Yay! You win') 
+       break
+
+
+
+
 
 # user_name = input("Please insert your name: ")
 # while len(user_name) < 1 or user_name.isalpha() is False:
